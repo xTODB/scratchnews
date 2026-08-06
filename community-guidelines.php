9bd064cd9087332aaa9730c842a041b7e7e02e6a
xTODB <davidas.brinzila@gmail.com>
2026-08-06 21:17:44 +0300

    a

--- community-guidelines.php
+++ community-guidelines.php
@@ -8,7 +8,7 @@ startSession();
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="icon" type="image/svg+xml" href="/assets/favicon.svg">
-<title>About - <?= e(SITE_NAME) ?></title>
+<title>Community Guidelines - <?= e(SITE_NAME) ?></title>
 <link rel="stylesheet" href="/assets/style.css?v=9">
 </head>
 <body <?php include __DIR__ . '/includes/theme-body.php'; ?>>
