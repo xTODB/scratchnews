b83132162349f1dbf86e82f6bf10aaf1e26610fb
TODB <davidas.brinzila@gmail.com>
2026-08-21 11:16:10 +0300

    a

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 // === MAINTENANCE MODE (git-tracked, no File Manager needed) ===
 date_default_timezone_set('Etc/GMT-3'); // GMT+3
-define('MAINTENANCE_MODE', true);
+define('MAINTENANCE_MODE', false);
 define('MAINTENANCE_UNTIL', null); // null = stays on until you flip this back to false
-define('MAINTENANCE_MESSAGE', 'TEST MAINTENANCE SITE WILL BE UP IN 52 SECONDS OR LESS');
+define('MAINTENANCE_MESSAGE', 'maintenance');
 
 $maintenance_active = MAINTENANCE_MODE && (MAINTENANCE_UNTIL === null || time() < strtotime(MAINTENANCE_UNTIL));
 
