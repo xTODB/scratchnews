ec7e166744004c05ae5984ef2eb142e82c27fc72
TODB <davidas.brinzila@gmail.com>
2026-08-22 07:46:42 +0300

    a

--- changelog.php
+++ changelog.php
@@ -16,6 +16,7 @@ startSession();
 <?php include __DIR__ . '/includes/header.php'; ?>
 <main>
     <h2>Changelog</h2>
+    <p>[Aug22] v0.23.3 - Fixed Comment Thread bug, Edit and Unpublish your Articles once they're Posted, Reply to Feedback, Images on Feedback, Polls (only I can make them right now haha)</p>
     <p>[Aug21] v0.23.2 - Maintenance mode, moved site to IFastNet, Comment Auth, API works normally</p>
     <p>[Aug19] v0.23.1 - Comma formatting on numbers bigger than 999, Related Articles + Extra Articles, hover toolbar on the three big on the homepage and Explore (like/dislike/share/comment/writer's profile), three-dot menu on tiny explore and search articles.</p>
     <p>[Aug16] v0.23 - Share ID (SID) system when sharing articles, badges when shares get clicks, share banners, share badges, fresh new Stats and Admin Stats pages, icon nav changed on mobile to bottom bar</p>
