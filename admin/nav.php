e78e471019041d9d17b8c25b06e3ab83832a032a
TODB <davidas.brinzila@gmail.com>
2026-09-05 22:50:38 +0300

    Add 'Forums' link to admin navigation

--- admin/nav.php
+++ admin/nav.php
@@ -17,6 +17,7 @@
             <a href="/admin/move">Move Content</a>
             <a href="/admin/moderation-words">Moderation Words</a>
             <a href="/admin/banners">Banners</a>
+            <a href="/admin/forums">Forums</a>
             <a href="/admin/polls">Polls</a>
             <a href="/admin/group-requests">Group Requests<?php $c = getPendingGroupRequestsCount(); if ($c > 0): ?><span class="admin-nav-badge"><?= $c ?></span><?php endif; ?></a>
             <a href="/admin/stats">Stats</a>
